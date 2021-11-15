2e388609b2a6316b6cee7918374fe92438a18b76
Ariel <ariel1242009@gmail.com>
2021-11-15 05:05:06 -0300

    correções e ajustes finos feitos

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@
 let username = "";
 let nome = {};
 let userRegistration;
+let message_to = "Todos";
+let visibility = "message";
+let intervalUser;
+let intervalChat;
 function userReg(){
+  clearInterval(intervalUser);
   username = prompt("Insira o seu nome:");
   nome = {
     name: username
@@ -14,11 +19,11 @@ function userReg(){
 loadPage();
 const all = `
   <li>
-    <div class="flex">
-      <span class="button-send"></span>
+    <div class="flex" onclick="messageTo(this)">
+      <ion-icon class="all-icon" name="people" onclick="showPeople()"></ion-icon>
       <span class="all">Todos</span>
     </div>
-    <span class=""></span>
+    <ion-icon class="check" name="checkmark-sharp"></ion-icon>
   </li>
 `;
 //lendo o enter
@@ -38,14 +43,15 @@ function showPeople() {
 }
 //mantendo a conexão
 function fixOn(){
-  setInterval(function(){
+  intervalUser = setInterval(function(){
     axios.post("https://mock-api.driven.com.br/api/v4/uol/status", nome)
   }, 5000);
 }
 //atualizando o chat
 function loadPage(){
+  clearInterval(intervalChat);
   axios.get("https://mock-api.driven.com.br/api/v4/uol/messages").then(pullMessages);
-  setInterval(function(){
+  intervalChat = setInterval(function(){
     axios.get("https://mock-api.driven.com.br/api/v4/uol/messages").then(pullMessages);
   }, 3000);
   userReg();
@@ -55,34 +61,59 @@ function sendMsg(){
   let msg = document.querySelector(".footer input");
   let mensagem = {
     from: username,
-    to: "Todos",
+    to: message_to,
     text: msg.value,
-    type: "message"
+    type: visibility
   };
   let testinho = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", mensagem);
+  msg.value = "";
   testinho.catch(attPage);
 }
 
 function attPage(){
   window.location.reload();
-  /* loadPage(); */
   userReg();
 }
 //ATUALIZANDO LISTA USUÁRIOS
-const intervalUsers = setInterval(loadUsers, 10000);
+let intervalUsers = setInterval(loadUsers, 10000);
 //FUNÇÃO P/ IMPRIMIR CHAT
 function pullMessages(resposta) {
   document.querySelector(".chat ul").innerHTML = "";
   for(let i = 0; i < resposta.data.length; i++) {
-    document.querySelector(".chat ul").innerHTML += `
-    <li class="${resposta.data[i].type}">
-      <span class="time light">(${resposta.data[i].time})</span> <span class="name-user bold">${resposta.data[i].from}</span> ${resposta.data[i].text}
-    </li>
-    `;
+    if(resposta.data[i].type === "message"){
+      document.querySelector(".chat ul").innerHTML += `
+      <li class="message" data-identifier="message">
+      <span class="time light">(${resposta.data[i].time})</span> 
+      <span class="name-user bold">${resposta.data[i].from}</span> paraa <span class="name-user bold">${message_to}</span> ${resposta.data[i].text}
+      </li>
+      `;
+    } else if(resposta.data[i].type === "private_message" && (resposta.data[i].from == username || resposta.data[i].to == username)){
+      if(resposta.data[i].to == "Todos"){
+        document.querySelector(".chat ul").innerHTML += `
+        <li class="message" data-identifier="message">
+        <span class="time light">(${resposta.data[i].time})</span> 
+        <span class="name-user bold">${resposta.data[i].from}</span> para <span class="name-user bold">${message_to}</span> ${resposta.data[i].text}
+        </li>
+        `;
+      } else {
+        document.querySelector(".chat ul").innerHTML += `
+        <li class="private_message" data-identifier="message">
+        <span class="time light">(${resposta.data[i].time})</span> 
+        <span class="name-user bold">${resposta.data[i].from}</span> para <span class="name-user bold">${message_to}</span> ${resposta.data[i].text}
+        </li>
+        `;
+      }
+    } else if(resposta.data[i].type === "status") {
+      document.querySelector(".chat ul").innerHTML += `
+      <li class="${resposta.data[i].type}" data-identifier="message">
+      <span class="time light">(${resposta.data[i].time})</span> 
+      <span class="name-user bold">${resposta.data[i].from}</span> ${resposta.data[i].text}
+      </li>
+      `;
+    }
   }
-  let lastMessage = document.querySelector(".chat ul").children[99];
+  let lastMessage = document.querySelector(".chat ul").children[document.querySelector(".chat ul").children.length - 1];
   lastMessage.scrollIntoView();
-  console.log("atualizei");
 }
 //Buscando usuários e imprimindo
 function loadUsers(){
@@ -92,14 +123,41 @@ function loadUsers(){
     document.querySelector(".users ul").innerHTML = all;
     for(let i = 0; i < resposta.data.length; i++) {
       document.querySelector(".users ul").innerHTML += `
-        <li>
-          <div class="flex">
-            <span class="button-send"></span>
+        <li data-identifier="participant">
+          <div class="flex" onclick="messageTo(this)">
+            <ion-icon class="user-icon" name="person-circle"></ion-icon>
             <span class="user">${resposta.data[i].name}</span>
           </div>
-          <span class=""></span>
+          <ion-icon class="check" name="checkmark-sharp"></ion-icon>
         </li>
       `;
+    }
+    checkDisplay();
+  }
+}
+function messageTo(element){
+  if(element.children[1].innerHTML != username && element.children[1].innerHTML != message_to){
+    message_to = element.children[1].innerHTML;
+    document.querySelector(".users ul li .check.display").classList.remove("display");
+    element.parentNode.children[1].classList.add("display");
+  }
+}
+function visibleTo(element){
+  if(element.children[1].innerHTML == "Público"){
+    visibility = "message";
+    element.parentNode.children[1].classList.add("display");
+    document.querySelector(".lock").parentNode.parentNode.children[1].classList.remove("display");
+  } else if(message_to != "Todos") {
+    visibility = "private_message";
+    element.parentNode.children[1].classList.add("display");
+    document.querySelector(".unlock").parentNode.parentNode.children[1].classList.remove("display");
+  }
+}
+function checkDisplay() {
+  let element = document.querySelectorAll(".users ul li");
+  for(let i = 0; i < element.length; i++){
+    if(element[i].children[0].children[1].innerHTML == message_to){
+      element[i].children[1].classList.add("display");
     }
   }
 }
